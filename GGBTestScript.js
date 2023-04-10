20332e3b19871b7db4348ad3af35c246d5f671a7
Kimberly Hager <hager.kimberly.a@gmail.com>
2023-04-10 19:14:59 -0400

    Quotes

--- GGBTestScript.js
+++ GGBTestScript.js
@@ -1,4 +1,4 @@
 ggbObject = ggbApplet;
 console.log("fetched, but with more comments");
 ggbObject.evalCommand("A=(5,3)");
-ggbObject.evalCommand("text1=I\\'m adding text here by writing in VS Code.");
+ggbObject.evalCommand('text1="I\'m adding text here by writing in VS Code."');
