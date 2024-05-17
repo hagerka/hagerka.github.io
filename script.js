727745876dd7b21917b062bb8d41d57ea35c5273
Kimberly Hager <hager.kimberly.a@gmail.com>
2024-05-16 22:04:45 -0400

    Updates styles and Tuva

--- script.js
+++ script.js
@@ -10,7 +10,17 @@ speakerIcon.addEventListener("click", () => {
 muteIcon.addEventListener("click", () => {
   speechSession.cancel();
   let utterance = new SpeechSynthesisUtterance("Speech turned on");
+  let lastUtterance = "";
   speechSynthesis.speak(utterance);
   speakerIcon.classList.remove("muted");
   muteIcon.classList.add("unmuted");
+});
+
+document.body.addEventListener("mousemove", evt => {
+  let lastUtterance = "";
+  let utterance = new SpeechSynthesisUtterance(evt.target.innerText);
+  if (utterance !== lastUtterance) {
+    speechSynthesis.speak(utterance);
+    utterance = lastUtterance;
+  }
 });
