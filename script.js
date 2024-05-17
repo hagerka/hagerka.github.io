683182c85c14624b38ded346a0e5c94b6213a91b
Kimberly Hager <hager.kimberly.a@gmail.com>
2024-05-16 22:46:36 -0400

    Fixes audio readout

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ const speakerIcon = document.querySelector("#speaker-icon");
 const muteIcon = document.querySelector("#mute-icon");
 const speechSession = window.speechSynthesis;
 
+let flag = false;
 speakerIcon.addEventListener("click", () => {
+  flag = false;
   speakerIcon.classList.add("muted");
   muteIcon.classList.remove("unmuted");
 });
 
 muteIcon.addEventListener("click", () => {
   speechSession.cancel();
+  flag = true;
   let utterance = new SpeechSynthesisUtterance("Speech turned on");
-  let lastUtterance = "";
   speechSynthesis.speak(utterance);
   speakerIcon.classList.remove("muted");
   muteIcon.classList.add("unmuted");
 });
 
-document.body.addEventListener("mousemove", evt => {
+document.body.addEventListener("mouseover", evt => {
+  speechSession.cancel();
   let lastUtterance = "";
   let utterance = new SpeechSynthesisUtterance(evt.target.innerText);
-  if (utterance !== lastUtterance) {
+  if (utterance !== lastUtterance && flag) {
     speechSynthesis.speak(utterance);
     utterance = lastUtterance;
   }
